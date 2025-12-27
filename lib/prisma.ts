66181ac5ee418dbadebd8d77b2947658bf36367c
Emad Saeed <hello@emadsaeed.com>
2025-12-27 22:02:48 +0000

    Database added-2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-function makeClient() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
   });
-
-  const adapter = new PrismaPg(pool);
-
-  return new PrismaClient({ adapter });
-}
-
-export const prisma = globalForPrisma.prisma ?? makeClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
